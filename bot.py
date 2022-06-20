30123791865489ac8c6dda38458397a676a67d88
MGASALUCAS <maulidiadam1234@gmail.com>
2022-06-20 21:43:10 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,87 @@
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CommandHandler
+from dictionary import get_info
+import os
+
+telegram_bot_token = '5599957095:AAGIPiYBZmoftR0cF4pjzwwmbMc1db3P4TA'
+
+updater = Updater(token=telegram_bot_token, use_context=True)
+dispatcher = updater.dispatcher
+
+
+# set up the introductory statement for the bot when the /start command is invoked
+def start(update, context):
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id=chat_id, text="Hello there. Provide any English word and I will give you a bunch "
+                                                   "of information about it.")
+
+
+# obtain the information of the word provided and format before presenting.
+def get_word_info(update, context):
+    # get the word info
+    word_info = get_info(update.message.text)
+
+    # If the user provides an invalid English word, return the custom response from get_info() and exit the function
+    if word_info.__class__ is str:
+        update.message.reply_text(word_info)
+        return
+
+    # get the word the user provided
+    word = word_info['word']
+
+    # get the origin of the word
+    origin = word_info['origin']
+    meanings = '\n'
+
+    synonyms = ''
+    definition = ''
+    example = ''
+    antonyms = ''
+
+    # a word may have several meanings. We'll use this counter to track each of the meanings provided from the response
+    meaning_counter = 1
+
+    for word_meaning in word_info['meanings']:
+        meanings += 'Meaning ' + str(meaning_counter) + ':\n'
+
+        for word_definition in word_meaning['definitions']:
+            # extract the each of the definitions of the word
+            definition = word_definition['definition']
+
+            # extract each example for the respective definition
+            if 'example' in word_definition:
+                example = word_definition['example']
+
+            # extract the collection of synonyms for the word based on the definition
+            for word_synonym in word_definition['synonyms']:
+                synonyms += word_synonym + ', '
+
+            # extract the antonyms of the word based on the definition
+            for word_antonym in word_definition['antonyms']:
+                antonyms += word_antonym + ', '
+
+        meanings += 'Definition: ' + definition + '\n\n'
+        meanings += 'Example: ' + example + '\n\n'
+        meanings += 'Synonym: ' + synonyms + '\n\n'
+        meanings += 'Antonym: ' + antonyms + '\n\n\n'
+
+        meaning_counter += 1
+
+    # format the data into a string
+    message = f"Word: {word}\n\nOrigin: {origin}\n{meanings}"
+
+    update.message.reply_text(message)
+
+
+# run the start function when the user invokes the /start command
+dispatcher.add_handler(CommandHandler("start", start))
+
+# invoke the get_word_info function when the user sends a message
+# that is not a command.
+dispatcher.add_handler(MessageHandler(Filters.text, get_word_info))
+updater.start_webhook(listen="0.0.0.0",
+                      port=int(os.environ.get('PORT', 5000)),
+                      url_path=telegram_bot_token,
+                      webhook_url=  + telegram_bot_token
+                      )
---
+++ dictionary.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_info(word):
+
+    url = 'https://api.dictionaryapi.dev/api/v2/entries/en/{}'.format(word)
+
+    response = requests.get(url)
+
+# return a custom response if an invalid word is provided
+    if response.status_code == 404:
+        error_response = 'We are not able to provide any information about your word. Please confirm that the word is ' \
+                         'spelled correctly or try the search again at a later time.'
+        return error_response
+
+    data = response.json()[0]
+
+    print(data)
+    return data
+
+
+get_info("food")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
